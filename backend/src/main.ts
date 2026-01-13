9bb7ba04686a27133a0ab7e53192d9e3e98dd21c
Mykhailo <kurochkinmy@gmail.com>
2026-01-13 18:06:50 +0200

    fix: fix CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3001);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
